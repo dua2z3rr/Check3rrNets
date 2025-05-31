7e698814c44c6f11420c02c548672a04710d4dc6
dua2z3rr <dua2z3rr@gmail.com>
2025-05-31 17:07:36 +0200

    implemented scapy, fullscan mode TCP scan operational

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
-help_message = """-t: Specify target (IP, IP range or subnet)
--p: Define ports to scan (single, range or list)
+from scapy.all import *
+from scapy.layers.l2 import Ether
+from scapy.layers.inet import IP, ICMP, TCP
+from sympy import false
+
 help_message = """-t: Specify target (IP, IP range or subnet) (default: loopback)
 -p: Define ports to scan (single, range or list) (default: all)
 -n: Scan top N common ports (default: 100)
@@ -7,7 +10,7 @@ help_message = """-t: Specify target (IP, IP range or subnet) (default: loopback
 -T: TCP Connect scan
 -b: Activate service banner grabbing
 -j: Number of parallel threads (default: 50)
--w: Connection timeout (default: 3 seconds)
+-w: TimeBetweenPackets (default: 3 seconds)
 -x: Exclude specified ports
 -O: Show only open ports
 -o: Output results file
@@ -20,18 +23,27 @@ help_message = """-t: Specify target (IP, IP range or subnet) (default: loopback
 -R: Random port order
 -l: Preset scan list (fast/complete)"""
 
-RHOSTS = ["127.0.0.1"]
-TIMEOUT = 3
+global RHOSTS
+global TimeBetweenPackets
+global FULLSCAN
+global PORTS
+
+RHOSTS = []
+TimeBetweenPackets = 0.2
 FULLSCAN = True
 PORTS = []
 
+conf.verb = 0
+
 def start():
+    global RHOSTS, TimeBetweenPackets, FULLSCAN, PORTS
     input_str = input("insert options for scan (-h for help):")
     options = [opt for opt in input_str.split("-") if opt.strip() != ""]
     print(options)
     return options
 
 def optionsEvaluation(options):
+    global RHOSTS, TimeBetweenPackets, FULLSCAN, PORTS
     for option in options:
         optionArray = option.split(" ")
         match optionArray[0]:
@@ -40,11 +52,13 @@ def optionsEvaluation(options):
                 return 0;
             case "t":
                 for ip in optionArray[1:]:
-                    RHOSTS.append(ip)
+                    if(ip != " "):
+                        RHOSTS.append(ip)
             case "p":
                 FULLSCAN = False
                 for port in optionArray[1:]:
-                    PORTS.append(int(port))
+                    if(port != " "):
+                        PORTS.append(int(port))
             case "n":
                 print()
             case "S":
@@ -58,6 +72,7 @@ def optionsEvaluation(options):
             case "w":
                 if(optionArray[1].type() == int | optionArray[1].type() == float):
                     TIMEOUT = optionArray[1]
+                    FULLSCAN = False
                 else:
                     print("MUST BE A NUMBER!")
             case "x":
@@ -86,35 +101,33 @@ def optionsEvaluation(options):
     return None
 
 def scan():
-    socket.setdefaulttimeout(TIMEOUT)
-    s = socket.socket()
-    i = 0
+    global RHOSTS, TimeBetweenPackets, FULLSCAN, PORTS
 
     for RHOST in RHOSTS:
         print("-------------------- connecting to %s --------------------" % RHOST)
 
         if(FULLSCAN):
-            for i in range(1, 65566):
-                if(i == 1):
-                    print("-------------------- WELL-KNOWN PORTS --------------------")
-                elif(i == 1024):
-                    print("-------------------- REGISTERED PORTS --------------------")
-                elif(i == 49152):
-                    print("-------------------- DYNAMIC PORTS --------------------")
-                try:
-                    s.connect((RHOST, int(i)))
-                    x = s.recv(1024)
-                    print(x)
-                    abe = x[24:29]
-                    print(abe)
-                finally:
-                    s.close()
+            print("fullscan mode")
+            for i in range(65545):
+                ip = IP(dst=RHOST)  # Target IP
+                tcp = TCP(dport=i, flags="S")
+                packet = ip / tcp  # Full packet
+                send(packet, count=1, inter=TimeBetweenPackets, verbose=0)
+                response = sr1(packet, timeout=2)  # Send and wait for one reply
+                if response and response.haslayer(TCP) and response[IP].src == RHOST and response[TCP].flags == 0x12:  # SYN-ACK ricevuto dal target
+                    print(f"Port {tcp.dport} open")
+
         else:
+            print("NOT fullscan mode")
             for i in PORTS:
-                if(i == 1024):
-                    print("-------------------- REGISTERED PORTS --------------------")
-                elif(i == 49152):
-                    print("-------------------- DYNAMIC PORTS --------------------")
+                #eth = Ether(dst="?????????")  # Ethernet broadcast
+                ip = IP(dst=RHOST)  # Target IP
+                icmp = ICMP(type=8)  # ICMP layer TODO
+                packet = ip / icmp  # Full packet
+
+                print(packet)
+
+                send(packet, count=10, inter=TimeBetweenPackets)
 
 
 optionsEvaluation(start())
